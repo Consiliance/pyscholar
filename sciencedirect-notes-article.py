6ef26a8eace974c3e0c7f60a184aa3a2428c8e76
kanzure <kanzure@gmail.com>
2009-07-25 20:14:28 -0500

    more xpaths

---
+++ sciencedirect-notes-article.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+
+#paper title: /html/body/div/div/div[@id='sdBody']/div[2]/div/div[@id='articleBody']/div[@id='articleHeader']/div[2]
+#authors: /html/body/div/div/div[@id='sdBody']/div[2]/div/div[@id='articleBody']/div[@id='articleHeader']/strong/p
+#abstract: /html/body/div/div/div[@id='sdBody']/div[2]/div/div[@id='articleBody']/div[@id='articleHeader']/div[8]/p
+#keywords: /html/body/div/div/div[@id='sdBody']/div[2]/div/div[@id='articleBody']/div[@id='articleHeader']/div[9]/p
+#pdf link: /html/body/div/div/div[@id='sdBody']/div[2]/div/table[1]/tbody/tr/td[1]/a/div/div
+#journal name: /html/body/div/div/div[@id='sdBody']/table/tbody/tr/td[1]/div[@id='artihead']/a[1]/b
+#volume/issue: /html/body/div/div/div[@id='sdBody']/table/tbody/tr/td[1]/div[@id='artihead']/a[2]
+#DOI number/link: /html/body/div/div/div[@id='sdBody']/div[2]/div/div[@id='articleBody']/div[@id='articleHeader']/a
+#how to get page numbers: /html/body/div/div/div[@id='sdBody']/table/tbody/tr/td[1]/div[@id='artihead']
+#Cited By link: /html/body/div/div/div[@id='sdBody']/div[2]/div/table[2]/tbody/tr[2]/td/div/table/tbody/tr/td[1]/table/tbody/tr[2]/td/a
+#Export Citation link: /html/body/div/div/div[@id='sdBody']/div[2]/div/table[2]/tbody/tr[2]/td/div/table/tbody/tr/td[2]/table/tbody/tr[1]/td/a
+#bibliography/references: /html/body/div/div/div[@id='sdBody']/div[2]/div/div[@id='articleBody']/div[@id='articleHeader']/div[19]/div
+
+
+
+
+
